3a12d9f706eb605347677fde80112e01b6c900ec
Alexchiper1 <alexchiper20@gmail.com>
2026-09-23 16:04:49 +0100

    added validation using regex for both name and password

--- app/src/main/java/com/example/userform/MainActivity.java
+++ app/src/main/java/com/example/userform/MainActivity.java
@@ -29,7 +29,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void submit(View view) {
         EditText txt = findViewById(R.id.name);
+        EditText phone = findViewById(R.id.phone);
+
+
         String name = txt.getText().toString();
+        String mobile = phone.getText().toString();
+
+        if(!name.matches("[a-zA-Z]+")){
+            txt.setError("Name must be a character");
+            txt.requestFocus();
+        }
+
+        if(mobile.matches("[a-zA-Z]+")){
+            phone.setError("Phone must only contain numbers");
+            phone.requestFocus();
+        }
+
         Toast.makeText(this, "Thank you "+ name +", your request is being processed", Toast.LENGTH_SHORT).show();
     }
 }
